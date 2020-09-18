1bb8ac5219cf6ef9709402fb2e2684a04d835f42
handrawanw <handrawanw@gmail.com>
2020-09-19 00:10:01 +0700

    Updates

---
+++ precache-manifest.c80555d11a6cf7396fad93d8ce6aa906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a599e8faef605a8e3d3485cb13c4b171",
+    "url": "/trading/index.html"
+  },
+  {
+    "revision": "a51ca3fc5bc6d64e58de",
+    "url": "/trading/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "02a31093c0edee226e03",
+    "url": "/trading/static/css/main.fb08442a.chunk.css"
+  },
+  {
+    "revision": "a51ca3fc5bc6d64e58de",
+    "url": "/trading/static/js/2.a30945d9.chunk.js"
+  },
+  {
+    "revision": "ad64764f5b3563c1a3c2da08db2000df",
+    "url": "/trading/static/js/2.a30945d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a31093c0edee226e03",
+    "url": "/trading/static/js/main.cb958b98.chunk.js"
+  },
+  {
+    "revision": "fed5d81f595cfba91359",
+    "url": "/trading/static/js/runtime-main.fcf35010.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d678f1b90a9b2d7d9978007fb0ba2f37.js"
+  "/trading/precache-manifest.c80555d11a6cf7396fad93d8ce6aa906.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trading/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

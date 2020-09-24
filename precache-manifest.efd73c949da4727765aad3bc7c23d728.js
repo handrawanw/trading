54bb822cc723fc9fe99cfc1c7df4efba250d86a8
handrawanw <handrawanw@gmail.com>
2020-09-24 14:55:24 +0700

    Updates

--- precache-manifest.efd73c949da4727765aad3bc7c23d728.js
+++ precache-manifest.efd73c949da4727765aad3bc7c23d728.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4c9798232b28b3c0e5b3306a4a71a229",
+    "revision": "986927ee0346db8e0f7dc5dffc19f6d6",
     "url": "/trading/index.html"
   },
   {
-    "revision": "094bc629f843d301fb34",
+    "revision": "621fa525292fbdd30bac",
     "url": "/trading/static/css/2.8aa5a7f8.chunk.css"
   },
   {
-    "revision": "d120d299cfb255a87f37",
+    "revision": "2c13b33ebe6c2b93be4d",
     "url": "/trading/static/css/main.40fc1966.chunk.css"
   },
   {
-    "revision": "094bc629f843d301fb34",
-    "url": "/trading/static/js/2.95d9460e.chunk.js"
+    "revision": "621fa525292fbdd30bac",
+    "url": "/trading/static/js/2.88f08362.chunk.js"
   },
   {
     "revision": "ad64764f5b3563c1a3c2da08db2000df",
-    "url": "/trading/static/js/2.95d9460e.chunk.js.LICENSE.txt"
+    "url": "/trading/static/js/2.88f08362.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d120d299cfb255a87f37",
-    "url": "/trading/static/js/main.c7174ed5.chunk.js"
+    "revision": "2c13b33ebe6c2b93be4d",
+    "url": "/trading/static/js/main.9d73745b.chunk.js"
   },
   {
     "revision": "fed5d81f595cfba91359",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trading/precache-manifest.ae6b6d57304c79fc4805023eabf58463.js"
+  "/trading/precache-manifest.efd73c949da4727765aad3bc7c23d728.js"
 );
 
 self.addEventListener('message', (event) => {

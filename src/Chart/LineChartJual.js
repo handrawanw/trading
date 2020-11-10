69832a55dfb5c068f0f5ae34f3037409d1daa31a
handrawanw <handrawanw@gmail.com>
2020-11-10 15:27:27 +0700

    .

--- src/Chart/LineChartJual.js
+++ src/Chart/LineChartJual.js
@@ -5,7 +5,9 @@ function AppChart(){
 
   const settings={
     series: [{
-      data: [{
+      name: 'candle',
+      data: [
+        {
           x: new Date(1538778600000),
           y: [6629.81, 6650.5, 6623.04, 6633.33]
         },
@@ -247,18 +249,44 @@ function AppChart(){
         },
       ]
     }],
-
     options: {
       chart: {
+        height: 350,
         type: 'candlestick',
-        height: "100%"
       },
       title: {
-        text: 'Gold Price',
+        text: 'CandleStick Chart - Category X-axis',
         align: 'left'
       },
+      annotations: {
+        xaxis: [
+          {
+            x: 'Oct 06 14:00',
+            borderColor: '#00E396',
+            label: {
+              borderColor: '#00E396',
+              style: {
+                fontSize: '12px',
+                color: '#fff',
+                background: '#00E396'
+              },
+              orientation: 'horizontal',
+              offsetY: 7,
+              text: 'Annotation Test'
+            }
+          }
+        ]
+      },
+      tooltip: {
+        enabled: true,
+      },
       xaxis: {
-        type: 'datetime'
+        type: 'category',
+        labels: {
+          formatter: function(val) {
+            return dayjs(val).format('MMM DD HH:mm')
+          }
+        }
       },
       yaxis: {
         tooltip: {
